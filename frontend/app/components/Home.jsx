989eeb1962c2d7296b205b061873885c3d97b61d
Sławomir Zborowski <slawomir.zborowski@nokia.com>
2015-12-31 19:49:53 +0100

    changed home page ; added some basic description about SimpleSTV

--- frontend/app/components/Home.jsx
+++ frontend/app/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import Header from './Header.jsx';
+import FullScreen from 'react-fullscreen';
 
 export default class Home extends Component {
     constructor(props) {
@@ -9,12 +9,20 @@ export default class Home extends Component {
 
     render() {
         return (
-            <div>
-                <Header text="Home" />
-                <div className="content">
-                    <Link to={'/p/create'}>+ new poll!</Link>
+            <FullScreen>
+                <div className="slight-background-gradient">
+                <div className="logo-wrapper">
+                    <img className="logo" src="./app/img/logo.png" />
                 </div>
-            </div>
+                <div className="main-page-wrapper">
+                    <h1>Welcome to SimpleSTV!</h1>
+                    <p>SimpleSTV is a platform for conducting Single Transferable Vote (STV) polls in as easy as possible manner. You can read more about STV <a href="https://en.wikipedia.org/wiki/Single_transferable_vote">here</a>.</p>
+                    <p>SimpleSTV is open source software. Source is available at <a href="https://github.com/szborows/simplestv">GitHub</a>.</p>
+                    <br />
+                    <Link to={'/p/create'} className="submit-button">Create new poll!</Link>
+                </div>
+                </div>
+            </FullScreen>
         );
     }
 };
