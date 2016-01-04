a4a7b1e8cdd0b68bdd5811790bd29cf3209503c8
Sławomir Zborowski <slawomir.zborowski@nokia.com>
2016-01-04 21:21:11 +0100

    added for loop in send_emails to send emails to multiple users..

--- backend/v1/tasks.py
+++ backend/v1/tasks.py
@@ -2,15 +2,17 @@ from __future__ import absolute_import
 from celery import shared_task
 from django.core.mail import send_mail
 from backend import settings
+from v1.views import hash_email # TODO: move to some util file...
 
 @shared_task
 def send_emails(poll, recipients):
-    body = """
-        Hi,
-        these links should work ;)
-        {}
-    """.format('\n'.join(['{0}/#/p/{1}/{2}'.format(settings.SIMPLESTV_URL , poll.hash_id, x.value) for x in poll.allowed_hashes.all()]))
-    send_mail('[simplestv-dev] subj', body, settings.DEFAULT_FROM_EMAIL, recipients, fail_silently=False)
+    for recipient in recipients:
+        body = """
+            Hi,
+            please vote using following link
+            {}
+        """.format('{0}/#/p/{1}/{2}'.format(settings.SIMPLESTV_URL, poll.hash_id, poll.allowed_hashes.get(value=hash_email(recipient))]))
+        send_mail('[simplestv-dev] subj', body, settings.DEFAULT_FROM_EMAIL, [recipient], fail_silently=False)
 
 @shared_task
 def run_election():
