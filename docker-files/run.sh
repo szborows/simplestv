e2295d4848a4679fa77e12d143ff1d704cd816c8
Sławomir Zborowski <slawomir.zborowski@nokia.com>
2016-01-05 02:03:55 +0100

    added semi-dev-semi-production Docker file

---
+++ docker-files/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd /app
+
+echo "Starting RabbitMQ server"
+echo "exit 0" > /usr/sbin/policy-rc.d
+invoke-rc.d rabbitmq-server start
+
+echo "Starting Celero worker"
+rm *.pid
+C_FORCE_ROOT=1 celery multi start w1 -A backend -l info
+
+echo "Starting Django application"
+mkdir /data
+python3.4 manage.py migrate --run-syncdb
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')\" | python3.4 manage.py shell" >> /tmp/createsuperuser
+chmod a+x /tmp/createsuperuser
+/tmp/createsuperuser
+echo "*       * * * * (python3.4 /app/manage.py send_mail >> ~/cron_mail.log 2>&1)" >> /etc/crontab
+echo "*       * * * * (python3.4 /app/manage.py retry_deferred >> ~/cron_mail_deferred.log 2>&1)" >> /etc/crontab
+
+uwsgi --socket :8001 --module backend.wsgi --daemonize /var/log/uwsgi.log
+
+/etc/init.d/nginx start
+
+cd /frontend
+npm start
