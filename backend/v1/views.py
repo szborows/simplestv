b4e0de4fa747cfc6af15e5730295c970893f5f93
Sławomir Zborowski <slawomir.zborowski@nokia.com>
2016-01-31 17:21:32 +0100

    poll view now robust to MultipleObjectsReturned

--- backend/v1/views.py
+++ backend/v1/views.py
@@ -21,9 +21,10 @@ class HttpResponseSeeOther(HttpResponseRedirect):
 def poll(request, poll_id):
     try:
         poll = Poll.objects.get(hash_id=poll_id)
-        # PARANOID: possible fuckup - more than one object returned
     except Poll.DoesNotExist:
         return HttpResponse(status=http.NOT_FOUND)
+    except Poll.MultipleObjectsReturned:
+        return HttpResponse(status=http.CONFLICT)
     try:
         key = request.GET['key']
     except KeyError:
