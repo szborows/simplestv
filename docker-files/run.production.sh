ab9398a9881079e6bcd965ce4ba670d217dafcf5
Sławomir Zborowski <slawomir.zborowski@nokia.com>
2016-02-04 00:37:38 +0100

    on the way to have PSQL data in custom dir

--- docker-files/run.production.sh
+++ docker-files/run.production.sh
@@ -2,12 +2,18 @@
 
 cd /app
 export SIMPLESTV_CONFIG_PATH="/app/config"
+echo "exit 0" > /usr/sbin/policy-rc.d
 
 echo "Starting PostgreSQL server"
+# TODO: move it to Dockerfile
+sed -i "s/data_directory.*/data_directory = '\/data\/postgres\'/g" /etc/postgresql/9.4/main/postgresql.conf
+chown -R postgres:postgres /data/postgres
+su - postgres -c "/usr/lib/postgresql/9.4/bin/initdb -D /data/postgres"
 /etc/init.d/postgresql start
+su - postgres -c "psql --command \"CREATE USER docker WITH SUPERUSER password 'docker';\" && createdb -O docker simplestv"
 
 echo "Starting RabbitMQ server"
-echo "exit 0" > /usr/sbin/policy-rc.d
+export RABBITMQ_BASE=/data/rabbitmq
 invoke-rc.d rabbitmq-server start
 
 cd /app/backend
@@ -18,7 +24,6 @@ rm *.log
 C_FORCE_ROOT=1 celery multi start w1 -A backend -l info
 
 echo "Starting Django application"
-mkdir /data
 python3.4 manage.py migrate --run-syncdb
 uwsgi --socket :8001 --module backend.wsgi --daemonize /var/log/uwsgi.log
 
